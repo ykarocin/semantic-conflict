95afb5f35e322315d8d6a6599f0ed92f8424c802
Ykaro dos Santos <110127975+ykarocin@users.noreply.github.com>
2025-02-21 12:50:29 -0300

    conflict

--- src/main/java/org/example/Confluence.java
+++ src/main/java/org/example/Confluence.java
@@ -1,6 +1,7 @@
 public class Confluence {
     public static void main(String[] args) {
         int x = 5;
+        ++x; //right
         int y = 10;
         int z = x + y;
         System.out.println("Result: " + z);
