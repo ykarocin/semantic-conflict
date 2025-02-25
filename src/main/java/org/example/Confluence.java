519cdf674dabdf5e0306636b3954f8f50d8b7639
ykarocin <ysa@cin.ufpe.br>
2025-02-25 11:58:31 -0300

    Adding confluence conflict to right

--- src/main/java/org/example/Confluence.java
+++ src/main/java/org/example/Confluence.java
@@ -4,6 +4,7 @@ public class Confluence {
     public static void main(String[] args) {
         int x = 5;
         int y = 10;
+        ++y; // right
         int z = x + y;
         System.out.println("Result: " + z);
     }
