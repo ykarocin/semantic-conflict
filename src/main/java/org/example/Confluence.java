fddbb9a28b5cb1bcb6fe37339d0bcb46a9e5a7b9
ykarocin <ysa@cin.ufpe.br>
2025-02-25 11:57:15 -0300

    Adding left conflict confluence

--- src/main/java/org/example/Confluence.java
+++ src/main/java/org/example/Confluence.java
@@ -3,6 +3,7 @@ package org.example;
 public class Confluence {
     public static void main(String[] args) {
         int x = 5;
+        ++x; //left
         int y = 10;
         int z = x + y;
         System.out.println("Result: " + z);
