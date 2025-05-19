b9218353aeb49619cf81f04e6c85adb0dcaea8bb
ykarocin <ysa@cin.ufpe.br>
2025-05-19 19:53:29 -0300

    updating oa base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         String input = "lorem ipsum";
         Text a = new Text(input);
-        a.cleanText();
+        a.generateReport();;
     }
 }
